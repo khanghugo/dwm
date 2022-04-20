c86dac50ff0a26505199fd81eccdf86d58958fe2
khang <khanghugo123@gmail.com>
2022-04-20 18:35:12 -0400

    fix one font and fix other components font

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack:size=11" , "Font Awesome 5 Free:size=12" , "Font Awesome 5 Brands:size=12"};
+static const char *fonts[]          = { "Hack:size=11" };
 static const char dmenufont[]       = "Hack:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
